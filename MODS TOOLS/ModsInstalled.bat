d3523b30a5669093db3a521165a95e31baff0384
Emanuelecaja18 <emanuelecaja18@gmail.com>
2023-12-24 14:02:56 +0100

    Added subfolder

--- MODS TOOLS/ModsInstalled.bat
+++ MODS TOOLS/ModsInstalled.bat
@@ -1,10 +1,8 @@
 @echo off
-setlocal enabledelayedexpansion
 
-set "file=mods.yml"
+set "file=../mods.yml"
 
 for /f "tokens=* delims=" %%a in (%file%) do (
-    set "line=%%a"
     echo %%a|find "displayName:" >nul
 	if not errorlevel 1 (echo %%a)
 )
